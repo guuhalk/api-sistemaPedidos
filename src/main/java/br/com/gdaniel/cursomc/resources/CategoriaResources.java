c017ceb4f1afd87f236232d2a06c1bbd70dfbbe1
Gustavo Daniel <gustavodaniel.santos@outlook.com>
2019-01-07 21:11:47 -0200

    Incluso classe categoria

--- src/main/java/br/com/gdaniel/cursomc/resources/CategoriaResources.java
+++ src/main/java/br/com/gdaniel/cursomc/resources/CategoriaResources.java
@@ -1,18 +1,36 @@
 package br.com.gdaniel.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.gdaniel.cursomc.model.Categoria;
 
 @RestController 
 @RequestMapping (value="/categorias")
 public class CategoriaResources {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest funfou hehe";
+	public List<Categoria> listar() {
 		
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Contabil");
+		Categoria cat3 = new Categoria(3,"Financeiro");
+		Categoria cat4 = new Categoria(4,"Fiscal");
+		Categoria cat5 = new Categoria(5,"Juridico");
+		Categoria cat6 = new Categoria(6,"Departamento Pessoal");
 		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		lista.add(cat3);
+		lista.add(cat4);
+		lista.add(cat5);
+		lista.add(cat6);
+		
+		return lista;
 	}
-	
 }
