e6fca56169a538f3f80fd350fb3d75586f080cdd
Gustavo Daniel <gustavodaniel.santos@outlook.com>
2019-04-15 19:39:17 -0300

    Correção JsonIgnore

--- src/main/java/br/com/gdaniel/cursomc/model/Pagamento.java
+++ src/main/java/br/com/gdaniel/cursomc/model/Pagamento.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import br.com.gdaniel.cursomc.Model.Enums.EstadoPagamento;
 
@@ -27,7 +27,7 @@ public abstract class Pagamento implements Serializable {
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
-	@JsonBackReference
+	@JsonIgnore
 	private Pedido pedido;
 	
 	
